2a07efbac0b3877817005c4c9307e058cb9ececc
Kerry <kerry.pellett@gmail.com>
2018-07-31 16:59:40 +1200

    Updated some files, removed most unneeded parts

--- Summative2/Summative2/scene.h
+++ Summative2/Summative2/scene.h
@@ -17,8 +17,8 @@ Mail        :   kerry.pel7420@mediadesign.school.nz
 // Local Includes
 #include "utility.h"
 #include "camera.h"
-#include "lighting.h"
-#include "map.h"
+
+
 
 // Forward declaration
 class CPlayer;
@@ -33,17 +33,11 @@ class CParticleSystem;
 class CScene {
 	protected:
 		std::unique_ptr<CCamera> m_pUICamera;
-		std::unique_ptr<CCubeMap> m_pSkybox;
-		std::vector<std::string> m_vecstrCubeMapTextures = { "right.png", "left.png", "top.png", "bottom.png", "back.png", "front.png" };
-		//std::vector<std::string> m_vecstrCubeMapTextures = { "right.jpg", "left.jpg", "top.jpg", "bottom.jpg", "back.jpg", "front.jpg" };
 
 	private:
 		// Game variables
-		int m_iMapIndex = 1;
 		std::unique_ptr<CCamera> m_pGameCamera;
 		glm::vec3 m_vfCameraOffset = glm::vec3(0.0f, 75.0f, 20.0f);
-		std::unique_ptr<CLighting> m_pLighting;
-		std::unique_ptr<CCube> m_pGround;
 		int m_iPlayerScore = 0;
 		std::unique_ptr<TextLabel> m_pScore = std::make_unique<TextLabel>(std::string("Score: 0"), "Resources/Fonts/calibri.ttf", glm::vec2(250, Utility::SCR_HEIGHT - 50));
 		std::unique_ptr<TextLabel> m_pLife = std::make_unique<TextLabel>(std::string("Life: 10"), "Resources/Fonts/calibri.ttf", glm::vec2(0, Utility::SCR_HEIGHT - 50));
@@ -53,15 +47,10 @@ class CScene {
 		bool m_bAITestReference = false;
 		bool m_bIsOnlineReference = false;
 		// Spawn locations
-		//std::unique_ptr<CPlayer> m_pPlayer;
 		std::vector<std::unique_ptr<CPlayer>> m_vecpPlayers;
-		std::vector<std::string> m_vecstrPlayerNames;
-		unsigned int m_uiClientInstance = 0;
 		std::vector<glm::vec3> m_vecPlayerSpawnPoints;
 		std::vector<glm::vec3> m_vecEnemySpawnPoints;
 		std::vector<glm::vec3> m_vecPickupSpawnPoints;
-		std::vector<std::unique_ptr<CCube>> m_vecpBarriers;
-		std::vector<std::unique_ptr<CCube>> m_vecpSpawnCube;
 		// Spawn timers
 		float m_fEnemySpawnTimer = 0.0f;
 		float m_fEnemySpawnDelay = 30.0f;
@@ -92,7 +81,7 @@ class CScene {
 		virtual void Render();
 		virtual void Process(float _fDeltaTick);
 
-		bool LoadMap(EMAPNODE _eMap[][11]);
+		//bool LoadMap(EMAPNODE _eMap[][11]);
 		bool Initialise(int _iMap);
 
 		glm::vec3 MouseRayToWorldSpace();
@@ -100,33 +89,12 @@ class CScene {
 		void SpawnBullet(glm::vec3 _vfPosition, glm::vec3 _vfVelocity, bool _bIsEnemy, unsigned int _uiOwner);
 		void SpawnPickup();
 
-		bool CheckForCollision(const CMesh* const _kpMesh1, const CMesh* const _kpMesh2);
+		bool CheckForCollision(const CEntity* const _kpMesh1, const CEntity* const _kpMesh2);
 		bool LoadSounds();
-		std::string GetPlayerPowerText()const;
+		//std::string GetPlayerPowerText()const;
 		std::unique_ptr<CPlayer>& FindClosestPlayer(glm::vec3 _AIPosition);
 		bool CheckForGameOver()const;
 
-		void UpdateClients();
-		void NotifyServer();
-		void SendGameData(char* _cPacketData);
-		void DeserialisePlayerPosition(unsigned int _iPlayerIndex, std::string _strPlayerPosition);
-		void DeserialisePlayerVelocity(unsigned int _iPlayerIndex, std::string _strPlayerPosition);
-		void DeserialisePlayerData(unsigned int _uiPlayerIndex, std::string _strPlayerData);
-		void DeserialisePickupData(unsigned int _iPickupIndex, std::string _strPickupdata);
-		void DeserialisePickupPosition(unsigned int _iPickupIndex, std::string _strPickupPosition);
-		void DeserialiseSoloAgentPosition(unsigned int _iAgentIndex, std::string _strPosition);
-		void DeserialiseGroupAgentPosition(unsigned int _iGroupIndex, unsigned int _iAgentIndex, std::string _strPosition);
-		void DeserialiseBulletPosition(unsigned int _uiBulletIndex, std::string _strData);
-		void DeserialiseBulletData(unsigned _uiBulletIndex, std::string _strData);
-		void DeserialiseBulletVelocity(unsigned int _uiBulletIndex, std::string _strData);
-		void DeserialiseSoloAgentData(unsigned int _uiAgentIndex, std::string _strData);
-		void DeserialiseGroupAgentData(unsigned int _uiGroupIndex, unsigned int _uiAgentIndex, std::string _strData);
-		void AffirmBulletCount(unsigned int _iBulletCount);
-		void CreatePlayerScores();
-		void ErasePlayer(unsigned int _uiPlayerIndex, unsigned int _uiNewLocalPlayerIndex);
-		void DeserialiseAndCreateBullet(std::string _strBulletInfo);
-		void CreateAndSerialiseBullet(glm::vec3 _vfPosition, glm::vec3 _vfVelocity, bool _bIsEnemy, unsigned int _uiOwner);
-		void SignalNextMap(unsigned int _uiMap);
 };
 
 
--- Summative2/Summative2/scenemanager.h
+++ Summative2/Summative2/scenemanager.h
@@ -18,9 +18,7 @@ Mail        :   kerry.pel7420@mediadesign.school.nz
 
 // Local includes
 #include "utility.h"
-#include "network.h"
-#include "client.h"
-#include "server.h"
+
 
 // Forward declaration
 class CScene;
@@ -42,29 +40,11 @@ class CSceneManager {
 		CSceneManager& operator=(const CSceneManager&) = delete;
 		static CSceneManager* m_pInstance;
 
-		// Network variables
-		float m_fLockStepTimer = 0.0f;
-
-		char* m_pcPacketData = nullptr;
-		char m_cIPAddress[MAX_ADDRESS_LENGTH];
-		std::string m_strServerName;
-		std::string m_strClientName;
-		CClient* m_pClient = nullptr;
-		CServer* m_pServer = nullptr;
-		std::thread m_tServerReceiveThread, m_tClientReceiveThread;
-
-		std::vector<std::string> m_vecstrPlayerNames;
-		bool m_bReadyToLaunch = false;
-		unsigned int m_uiClientInstance = 0;
-		bool m_bInProgress = false;
-		bool m_bIsOnline = false;
-
 		std::unique_ptr<CScene> m_pActiveScene;
 
 		int m_iScore = 0;
 		bool m_bPlayMusic = false;
 		bool m_bGodMode = true;
-		bool m_bAITest = false;
 
 
 	public:
@@ -80,7 +60,6 @@ class CSceneManager {
 		bool LoadGameMenu(EMENUTYPE _eMenu);
 
 		std::map<std::string, std::shared_ptr<Model>> m_pModels;
-		void PreloadModels();
 		void SetScore(int _iScore);
 		int GetScore()const;
 		void ToggleMusic();
@@ -90,52 +69,6 @@ class CSceneManager {
 		void ToggleAITest();
 		bool CheckForAITest()const;
 
-		// Networking
-		void SetServerName(std::string _strServerName);
-		std::string GetServerName()const;
-		std::vector<std::string> GetPlayerNames()const;
-
-		bool HostServer();
-		bool CreateClient();
-		void LaunchClientDataThread();
-		bool IsServerReadyToLaunch()const;
-		void LaunchServer();
-		bool IsDedicatedServer()const;
-		void ProcessWork();
-
-		CClient* GetClientInstance();
-		void SetInProgress(bool _bState);
-		bool CheckIfInProgress()const;
-		unsigned int GetNumberOfPlayers()const;
-
-		bool IsOnline()const;
-		void SetOnline(bool _bState);
-		void SendPlayerList();
-		void SetPlayerIndex(unsigned int _iIndex);
-		unsigned int GetPlayerIndex()const;
-
-		// Data exchange functions
-		void SendGameData(char* _cPacketData);
-		void SetPlayerLobbyNameList(std::vector<std::string> _vecStrNames);
-		void UpdateServerPlayerLobbyReadyState(int _iPlayerIndex, bool _bState);
-		void SetPlayerLobbyReadyState(int _iPlayerIndex, bool _bState);
-		// Gamestates
-		void DeserialisePlayerPosition(int _iPlayerIndex, std::string _strPosition);
-		void DeserialisePlayerVelocity(int _iPlayerIndex, std::string _strVelocity);
-		void DeserialisePlayerData(unsigned int _uiPlayerIndex, std::string _strData);
-		void DeserialisePickupData(unsigned int _iPickupIndex, std::string _strPickupData);
-		void DeserialisePickupPosition(unsigned int _iPickupIndex, std::string _strPickupPosition);
-		void DeserialiseSoloAgentPosition(unsigned int _iAgentPosition, std::string _strAgentPosition);
-		void DeserialiseGroupAgentPosition(unsigned int _iGroupIndex, unsigned int _iAgentIndex, std::string _strAgentPosition);
-		void DeserialiseBulletPosition(unsigned int _iBulletIndex, std::string _strData);
-		void DeserialiseBulletData(unsigned int _uiBulletIndex, std::string _strData);
-		void DeserialiseBulletVelocity(unsigned int _uiBulletIndex, std::string _strData);
-		void DeserialiseSoloAgentData(unsigned int _uiAgentIndex, std::string _strData);
-		void DeserialiseGroupAgentData(unsigned int _uiGroupIndex, unsigned int _uiAgentIndex, std::string _strData);
-		void AffirmBulletCount(unsigned int _uiBulletCount);
-		void DeserialiseAndCreateBullet(std::string _strBullet);
-		void RemovePlayer(unsigned int _iPlayerIndex, unsigned int _uiNewLocalPlayerIndex);
-		void LoadMap(unsigned int _uiMapIndex);
 };
 
 #endif // !__SCENEMANAGER_H__
